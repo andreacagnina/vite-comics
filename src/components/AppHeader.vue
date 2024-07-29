04d8d083d7aa2072607654f8c4a808ac7ec3b75e
andreacagnina <amirkal@libero.it>
2024-07-29 17:27:21 +0200

    [feat]header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -6,11 +6,55 @@ export default {
 
 <template>
   <header>
-    <i class="fas fa-envelope"></i>
-    <h1 class="text-center">
-      1
-    </h1>
+    <div class="container">
+      <div class="row">
+        <div class="col-2">
+        </div>
+        <div class="col-12">
+          <div class="d-flex justify-content-between align-items-baseline">
+            <div class="content bg-warning">
+              <a href="#">
+                <img src="../assets/dc-logo.png" alt="">
+              </a>
+            </div>
+
+            <div class="content bg-danger d-flex align-item-center">
+              <ul class="list-unstyled d-flex">
+                <li><a href="">1</a></li>
+                <li><a href="">2</a></li>
+                <li><a href="">3</a></li>
+                <li><a href="">4</a></li>
+                <li><a href="">5</a></li>
+                <li><a href="">6</a></li>
+                <li><a href="">7</a></li>
+                <li><a href="">8</a></li>
+                <li><a href="">9</a></li>
+                <li><a href="">10</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </header>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+header {
+  background-color: white;
+  padding: 20px;
+}
+
+ul {
+  li {
+    padding: 30px 30px;
+
+
+
+    a {
+      text-decoration: none;
+      color: black;
+    }
+  }
+}
+</style>
