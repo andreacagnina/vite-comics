4d633ae6538d9e167c5b0d1cea07a9ca0fabc451
andreacagnina <amirkal@libero.it>
2024-07-29 18:07:36 +0200

    [feat] top footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -6,8 +6,144 @@ export default {
 
 <template>
   <footer>
+    <div class="container">
+      <div class="row">
+        <div class="col-6">
+          <div class="content d-flex p-5">
+            <div class="col-4">
+              <div class="content">
+                <h4>
+                  dc comics
+                </h4>
+                <ul class="list-unstyled my-4">
+                  <li>
+                    Characters
+                  </li>
+                  <li>
+                    Comics
+                  </li>
+                  <li>
+                    Movies
+                  </li>
+                  <li>
+                    TV
+                  </li>
+                  <li>
+                    Games
+                  </li>
+                  <li>
+                    Videos
+                  </li>
+                  <li>
+                    News
+                  </li>
+                </ul>
+
+                <h4>
+                  shop
+                </h4>
+                <ul class="list-unstyled my-4">
+                  <li>Shop DC</li>
+                  <li>Shop DC Collectibles</li>
+                </ul>
+              </div>
+            </div>
+            <div class="col-4">
+              <div class="content">
+                <h4>dc</h4>
+                <ul class="list-unstyled my-4">
+                  <li>
+                    Terms Of Use
+                  </li>
+                  <li>
+                    Privacy policy(New)
+                  </li>
+                  <li>
+                    Add Choices
+                  </li>
+                  <li>
+                    Advertising
+                  </li>
+                  <li>
+                    Jobs
+                  </li>
+                  <li>
+                    Subscriptions
+                  </li>
+                  <li>
+                    Talent Workshops
+                  </li>
+                  <li>
+                    CPSC Certificates
+                  </li>
+                  <li>
+                    Ratings
+                  </li>
+                  <li>
+                    Shop Help
+                  </li>
+                  <li>
+                    Contact Us
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div class="col-4">
+              <div class="content">
+                <h4>sites</h4>
+                <ul class="list-unstyled my-4">
+                  <li>
+                    Characters
+                  </li>
+                  <li>
+                    DC
+                  </li>
+                  <li>
+                    MAD Magazine
+                  </li>
+                  <li>
+                    DC Kids
+                  </li>
+                  <li>
+                    DC Universe
+                  </li>
+                  <li>
+                    DC Power Visa
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+
+        </div>
+        <div class="col-6">
+          <div class="content">
+            <img src="/src/assets/dc-logo-bg.png" alt="">
+          </div>
+        </div>
+      </div>
+    </div>
 
   </footer>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+footer {
+  background-image: url(/src/assets/footer-bg.jpg);
+  background-repeat: no-repeat;
+  background-size: cover;
+  height: 500px;
+  color: white;
+
+  h4 {
+    text-transform: uppercase;
+  }
+
+  li {
+    color: gray
+  }
+
+  ;
+
+}
+</style>
