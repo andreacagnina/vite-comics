a5010a46276a6710f5538c57514522c95d429282
andreacagnina <amirkal@libero.it>
2024-07-30 11:17:04 +0200

    [feat] fix img big logo footer, need ticket height

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -173,9 +173,9 @@ export default {
     <div class="top-footer">
       <div class="container">
         <div class="row">
-          <div class="col-6">
-            <div class="content d-flex py-5">
-              <div class="col-4">
+          <div class="col-12 ">
+            <div class="content d-flex py-5 bg-image-logo">
+              <div class="col-2">
                 <div class="content">
                   <h4>
                     dc comics
@@ -194,7 +194,7 @@ export default {
                   </ul>
                 </div>
               </div>
-              <div class="col-4">
+              <div class="col-2">
                 <div class="content">
                   <h4>dc</h4>
                   <ul class="list-unstyled my-3">
@@ -202,7 +202,7 @@ export default {
                   </ul>
                 </div>
               </div>
-              <div class="col-4">
+              <div class="col-2">
                 <div class="content">
                   <h4>sites</h4>
                   <ul class="list-unstyled my-3">
@@ -212,11 +212,6 @@ export default {
               </div>
             </div>
 
-          </div>
-          <div class="col-6">
-            <div class="content bg-image-logo">
-              <img src="/src/assets/dc-logo-bg.png" alt="">
-            </div>
           </div>
         </div>
       </div>
@@ -265,14 +260,24 @@ footer {
 
 }
 
+.bg-image-logo {
+  background-image: url(/src/assets/dc-logo-bg.png);
+  background-repeat: no-repeat;
+
+  background-position: right;
+
+}
+
 .top-footer {
   background-image: url(/src/assets/footer-bg.jpg);
   background-repeat: no-repeat;
   background-size: cover;
+
 }
 
 .bottom-footer {
   background-color: #303030;
+
 
   li {
     padding: 20px 15px;
